9a33cdd54423eaa0c003a7a6d1651bb4867e98cf
ChrisK <chriskl@live.com>
2020-06-03 23:46:27 +0800

    signing off for the night and saving my progress

--- logic.js
+++ logic.js
@@ -1,15 +1,43 @@
+//Variables to Shorten text
 var startButton = document.getElementById('startbtn')
+var questionSection = document.getElementById('Question-Section')
+var questionElement = document.getElementById('question')
+var answersButtons = document.getElementById('Answer-Section')
+
+var shuffledQuestions, currentQuestionIndex
+
+
 //
-startButton.addEventListener('click', startgame)
+startButton.addEventListener('click', startQuiz)
 
 function startQuiz() {
-
+    console.log('Started')
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionSection.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
 
 }
 
 function selectAnswer() {
 
 }
+
+var questions = [
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            {text: '4', correct: true},
+            {text: '22', correct: false}
+        ]
+        
+    }
+]
