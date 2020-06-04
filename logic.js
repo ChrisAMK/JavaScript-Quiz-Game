4917886ec0e76f2ba486579436203045d89df268
ChrisK <chriskl@live.com>
2020-06-04 16:00:22 +0800

    Base game is finished, now need to implement rules and a timer

--- logic.js
+++ logic.js
@@ -5,13 +5,17 @@ var finishButton = document.getElementById('finishbtn')
 var introSection = document.getElementById('intro')
 var questionSection = document.getElementById('Question-Section')
 var questionElement = document.getElementById('question')
-var answersButtons = document.getElementById('Answer-Section')
+var answerButtons = document.getElementById('Answer-Section')
 
 var shuffledQuestions, currentQuestionIndex
 
 
 //
 startButton.addEventListener('click', startQuiz)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startQuiz() {
     console.log('Started')
@@ -22,22 +26,71 @@ function startQuiz() {
     nextButton.classList.remove('hide')
     finishButton.classList.remove('hide')
     introSection.classList.add('hide')
-
-
-
+    
     setNextQuestion()
 }
 
+function resetQuestions() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild
+        (answerButtons.firstChild)
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+        console.log("is this working?")
+    } else {
+        element.classList.add('wrong')
+        console.log("is this working?")
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
 function setNextQuestion() {
+    resetQuestions()
     showQuestion(shuffledQuestions[currentQuestionIndex])
+    
 }
 
 function showQuestion(question) {
-
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('button')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtons.appendChild(button)
+    })
 }
 
-function selectAnswer() {
 
+function selectAnswer(userPick) {
+    var selectedButton = userPick.target
+    var correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtons.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+    
+    
 }
 
 var questions = [
@@ -48,5 +101,30 @@ var questions = [
             {text: '22', correct: false}
         ]
         
+    },
+    {
+        question: 'What is 4 + 4',
+        answers: [
+            {text: '2', correct: false},
+            {text: '8', correct: true}
+        ]
+        
+    },
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            {text: '4', correct: true},
+            {text: '22', correct: false}
+        ]
+        
+    },
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            {text: '4', correct: true},
+            {text: '22', correct: false}
+        ]
+        
     }
 ]
+
